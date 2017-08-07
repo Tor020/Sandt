12a2be8c61dd13b58215769f2f35c8d6fc3a5d22
Kevin <Tor000020@gmail.com>
2017-08-07 07:07:43 -0400

    c
    c

--- scripts/main.js
+++ scripts/main.js
@@ -1,97 +1,109 @@
-document.body.addEventListener('click', function (e) {
-  
-    let atarget = e.target;
-    console.log(atarget);
-    });
+const canvas = document.getElementById('c');
 
-    let ms = document.querySelector('.mystuff');
-    let b = document.body;
-    ///////////////////
-    let dMaek = document.createElement('div');
-    let textnm = document.createTextNode('Maeked');
-    ///////////////////
-    dMaek.appendChild(textnm);
-    dMaek.className = 'brewn';
-    ///////////////////
-    // ms.appendChild(dMaek);
-    // ms.appendChild(dMaek);
-    // ms.appendChild(dMaek);
-
-///////////////////
-let canvas = document.getElementById('c');
+let c = canvas.getContext('2d');
+let w = canvas.width = window.innerWidth - 20;
+let h = canvas.height = window.innerHeight - 20;
 
 
-let can = {
+window.onload = function () {
 
-  clean:function (){
-    c.clearRect(0,0,canvas.width,canvas.height);
-},
-   ytho:function(idk){
-  return Math.floor((Math.random() * idk) + 1);
+  let circlesArr = [];
 
-},
-  mouse:function(e){
-    document.addEventListener('mousemove', 
-    function (e) {
-    let mx = e.clientX;
-    let my = e.clientY;
-      return;
-     });
-  },
-  draw: function () {
-    let canvas = document.getElementById('c');
-      c = canvas.getContext('2d');
-      w = canvas.width = window.innerWidth;-25;
-      h = canvas.height = window.innerHeight-25;
-      const centX = w / 2;
-      const centY = h / 2;
-      let posX = can.ytho(1820);
-      let posY = can.ytho(1000);
+  for (let i = 0; i < 50; i++) {
 
-     document.addEventListener('mousemove', function (e) {
-       mouseX  = e.clientX;
-       mouseY = e.clientY;
-     });
-
-    setInterval(function () {
-
-        let check = can.ytho(10);
-        (check <5) ? posX++ : posX--;
-        (check <5) ? posY++ : posY--;
-
-    
-      can.clean(); 
-      c.fillStyle = 'white';
-      ///
-
-      let circ1 = mouseX-50;
-      let circ2 = mouseX+50;
-      let circ3 = mouseY+50;
-      let circ4 = mouseY-50;
-      c.beginPath();
-      c.arc(circ1, mouseY, 6, 0, Math.PI * 2, true);
-      c.fill();
-      ///
-      c.beginPath();
-      c.arc(circ2, mouseY, 6, 0, Math.PI * 2, true);
-      c.fill();
-      ///
-      c.beginPath();
-      c.arc(mouseX, circ3, 6, 0, Math.PI * 2, true);
-      c.fill();
-      ///
-      c.beginPath();
-      c.arc(mouseX, circ4, 6, 0, Math.PI * 2, true);
-      c.fill();
-
-      for(circ1)
-    //  posX += velocX;
-    //  posY += velocY;
-
-      }
-    , 1000/60)
+    let iniX = Math.floor(Math.random() * (w - 92) + 60);
+    let iniY = Math.floor(Math.random() * (h - 92) + 60);
+    let iniVX = Math.ceil((Math.random() - 0.5) * 15);
+    let iniVY = Math.ceil((Math.random() - 0.5) * 15);
+    let radius = 15;
+    let id = [i];
+    circlesArr.push(new Circle(iniX, iniY, iniVX, iniVY, radius, id));
   }
-};
+console.log(circlesArr);
+  
+  function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, w, h);
+    for (let i = 0; i < circlesArr.length ; i++){
+      circlesArr[i].update();
+    }
+  }
+  animate();
+}
 
-can.draw();
+function Circle(x, y, vx, vy, radius, id, minSize, sizeLimit) {
 
+  
+  this.x = x;
+  this.y = y;
+  this.vx = vx;
+  this.vy = vy;
+  this.radius = randradi(25);
+  this.id = id;
+  this.sizeLimit = this.radius + 5;
+  this.minSize = this.radius;
+  this.color = colorArray[rand()];
+  this.draw = function () {
+
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, Math.PI * 2, false);
+
+    c.stroke();
+    c.fillStyle = this.color;
+    c.fill();
+  }
+  //Gets the initial random position
+      this.update = function () {
+
+
+        //Velocity added to x and y
+        this.x += this.vx;
+        this.y += this.vy;
+
+        //Walls
+        this.y > h - this.radius || this.y < this.radius ? this.vy = -this.vy : this.vy;
+        this.x > w - this.radius || this.x < this.radius ? this.vx = -this.vx : this.vx;
+
+        //When the mouse is with 50 pixels of any circle, it will execute
+    if (
+      mouse.x - this.x < 100 &&
+      mouse.x - this.x > -100 &&
+      mouse.y - this.y < 100 &&
+      mouse.y - this.y > -100 &&
+      this.radius < this.sizeLimit)
+    {
+      this.radius ++;
+    } else if (this.radius -10 > this.sizeLimit) {
+      console.log('wat')
+      this.radius --;
+    }
+
+    this.draw();
+  }
+}
+
+let mouse = {
+  x: undefined,
+  y: undefined
+}
+
+var colorArray = [
+  '#62C370',
+  '#9AD1D4',
+  '#C2F9BB',
+  '#D64550',
+  '#20063B', 
+];
+window.addEventListener('mousemove', function (e) 
+  {
+    mouse.x = event.x
+    mouse.y = event.y
+  
+  });
+
+  function rand() {
+    return Math.floor(Math.random() * colorArray.length); 
+  }
+  function randradi(a) {
+    return Math.floor((Math.random() * a) + 10); 
+  }
